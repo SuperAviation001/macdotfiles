d6dd5fea3f59ca999ded58087da909d96af867e7
SuperAviation001 <83483383+SuperAviation001@users.noreply.github.com>
2025-03-24 20:09:19 +1100

    initial commit, install cask from homebrew and cbonsai from macports before install

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+USER=$(id -un)
+
+if [ ! command -v brew >/dev/null 2>&1 ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Homebrew ✅"
+fi
+
+if ! brew list yadm >/dev/null; then
+    brew install yadm
+    echo "yadm ✅"
+fi
+
+if [ ! -f "$HOME/.config/yadm/bootstrap" ]; then
+    yadm clone https://github.com/ZhongXiLu/dotfiles.git --no-bootstrap
+fi
+
+yadm bootstrap
+
+if [ $USER == "zhongxilu" ]; then
+    yadm decrypt --yadm-archive "$HOME/.config/yadm/archive"
+fi
+
+if git config remote.faraway.url > /dev/null; then
+    yadm remote add origin git@github.com:ZhongXiLu/dotfiles.git
+fi
